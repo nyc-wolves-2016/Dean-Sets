61e600986eb4a637536d14fce3a420f42c708bab
Jack Feely & Jason Wong & Kimberly Suazo  & Soren Zeliger <pair+jackfeely.jwong411.kimberlysuazo.soreasy@devbootcamp.com>
2016-10-11 18:35:07 -0400

    adds logic for determining which cards to display and selecting and unselecting a card

--- app/assets/javascripts/components/Game.es6.jsx
+++ app/assets/javascripts/components/Game.es6.jsx
@@ -2,23 +2,39 @@ class Game extends React.Component {
 
   constructor() {
     super();
-
+    this.onButtonClick = this.onButtonClick.bind(this);
   }
 
   // change card's status
+  onButtonClick() {
+    // hit up ya boi controller for possible combos
 
+  }
 
   render() {
     let { deck } = this.props;
 
-    
+    var onBoardCards = deck.filter(card => card.status == "selected" || card.status == "onBoard");
+    var inDeckCards = deck.filter(card => card.status == "pending");
+
+    if (onBoardCards.length == 0) {
+      addNineCards(inDeckCards);
+    } else if (onBoardCards.length < 9) {
+      addThreeCards(inDeckCards);
+    }
+
+    var cardsToDisplay = deck.filter(card => card.status == "selected" || card.status == "onBoard");
 
     return(
-      <ul>
-        {deck.map((card, i) =>
-          <Card key={i} data={card} uponClick={this.props.uponClick}/>
-        )}
-      </ul>
+      <div>
+        <ul>
+          {cardsToDisplay.map((card, i) =>
+            <Card key={i} data={card} uponClick={this.props.uponClick}/>
+          )}
+        </ul>
+
+        <input type="button" name="ImaButton" onClick={this.onButtonClick} />
+      </div>
     )
   }
 }
