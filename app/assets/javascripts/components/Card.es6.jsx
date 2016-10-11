6736c36d2d549a15d6624fff399aa7c842d35f7b
Kimberly Suazo <email": null>
2016-10-11 19:08:12 -0400

    delete unnecessary comments

--- app/assets/javascripts/components/Card.es6.jsx
+++ app/assets/javascripts/components/Card.es6.jsx
@@ -6,11 +6,12 @@ class Card extends React.Component {
     let { color, shape, shading, number } = this.props.data;
     return(
       <li>
-        <div className="card highlight">
+        <div className="card">
           <div className="cardfront">
-            <div className='pill empty red '></div>
+            {/* <div className='pill empty red '></div>
             <div className='diamond lined purple'></div>
-            <div className='circle solid green'></div>
+            <div className='circle solid green'></div> */}
+            <p>{number} {shading} {color} {shape} {status}</p>
           </div>
         </div>
       </li>
--- app/assets/javascripts/components/Game.es6.jsx
+++ app/assets/javascripts/components/Game.es6.jsx
@@ -5,14 +5,10 @@ class Game extends React.Component {
     // this.onCardClick = this.onCardClick.bind(this);
   }
 
-
-
   render() {
     let { deck } = this.props;
-
     return(
       <div className="board">
-      <div className='circle red'></div>
         <ul>
           {deck.map((card, i) =>
             <Card key={i} data={card} />
