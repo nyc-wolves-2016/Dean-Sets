9211967642c6ec8e57d30130b2faf70685cac97b
Kimberly Suazo  & Soren Zeliger <pair+kimberlysuazo.soreasy@devbootcamp.com>
2016-10-25 18:02:59 -0400

    Move register and login forms to partials. Remove comments from user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,13 @@
 class UsersController < ApplicationController
   def login
-    # if logged_in?
-    #   @user = current_user
-    #   if request.xhr?
-    #     render json: @user.as_json
-    #   else
-    #     redirect_to @user
-    #   end
-    # else
-    #   session[:login] = true
-    #   render "login"
-    # end
   end
 
   def signin
     @user = User.find_by(email: user_params[:email])
     if @user && @user.authenticate(user_params[:password])
       session[:user_id] = @user.id
-      # if request.xhr?
-      #   render json: @user.as_json
-      # else
-      #   redirect_to @user
-      # end
     else
       @errors = ["Invalid email or password"]
-      # render 'login'
     end
   end
 
@@ -41,11 +24,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      # if request.xhr?
-      #   render json: @user.as_json
-      # else
-      #   redirect_to root_path
-      # end
     else
       @errors = @user.errors.full_messages
     end
